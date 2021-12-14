475d8021496a37564e65633a6fb9729de9d6796e
Miguel San Juan M <miguel.sanjuanm@gmail.com>
2021-12-14 10:19:37 -0300

    Initial Project structure

---
+++ src/main/java/com/benderbrewery/pricepoint/PricePointApplication.java
@@ -0,0 +1,13 @@
+package com.benderbrewery.pricepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricePointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricePointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benderbrewery/pricepoint/PricePointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benderbrewery.pricepoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricePointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
